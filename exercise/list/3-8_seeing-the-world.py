ebf515ccf55d3e66b1c2c9b16eb2dd0462d326bb
Adomous Wright <adomous.wright@uconn.edu>
2017-12-25 13:52:34 -0500

    Add more list exercises

---
+++ exercise/list/3-8_seeing-the-world.py
@@ -0,0 +1,32 @@
+# Author: Adomous Wright
+# Date: 12/25/17
+
+# Store and print list of locations
+
+locations = ['Boston','New York','Baltimore','Tokyo','Beijing']
+print(locations)
+
+# Print sorted list
+print(sorted(locations))
+
+# Show that the list is in original order
+print(locations)
+
+# Print the list in reverse order with sorted()
+print(sorted(locations, reverse=True))
+
+# Reverse and print list with reverse()
+locations.reverse()
+print(locations)
+
+# Reverse the list again and Print
+locations.reverse()
+print(locations)
+
+# Use sort() on list and print
+locations.sort()
+print(locations)
+
+# Use sort() to reverse the order
+locations.sort(reverse=True)
+print(locations)
---
+++ exercise/list/3-9_dinner-guests.py
@@ -0,0 +1,8 @@
+# Author: Adomous Wright
+# Date: 12/25/17
+
+# Make a guest list and print a message indicating the number of guests
+
+guests = ['Ryan','Patrick','Bob']
+num_guests = len(guests)
+print("There are", num_guests,"guests that will be attending the dinner party.")
